4c5d5929a910ed4bc4090b462c2c35217d2c1140
Vyacheslav Khorkov <artfintch@yandex.com>
2021-02-12 21:50:04 +0300

    Speed up integration step.

--- Sources/rugby/Commands/Cache/Integration/CacheIntegration.swift
+++ Sources/rugby/Commands/Cache/Integration/CacheIntegration.swift
@@ -14,38 +14,24 @@ struct CacheIntegration {
 
     func replacePathsToCache() throws {
         let supportFilesFolder = try Folder.current.subfolder(at: .podsTargetSupportFiles)
-
-        let buildedProductsDirToCacheFolder = buildedProducts.reduce(into: []) {
-            $0.append(("${BUILT_PRODUCTS_DIR}/\($1)", "\(cacheFolder)/\($1)"))
-        }
-        try replaceContent(occurrences: buildedProductsDirToCacheFolder,
-                           inFilesByRegEx: #".*-frameworks\.sh"#,
-                           folder: supportFilesFolder)
-
-        let podsConfigurationBuildDirToCacheFolder = buildedProducts.reduce(into: []) {
-            $0.append(("${PODS_CONFIGURATION_BUILD_DIR}/\($1)", "\(cacheFolder)/\($1)"))
-        }
-        try replaceContent(occurrences: podsConfigurationBuildDirToCacheFolder,
-                           inFilesByRegEx: #".*\.xcconfig"#,
-                           folder: supportFilesFolder)
-        try replaceContent(occurrences: buildedProductsDirToCacheFolder,
-                           inFilesByRegEx: #".*-resources\.sh"#,
-                           folder: supportFilesFolder)
-        try replaceContent(occurrences: podsConfigurationBuildDirToCacheFolder,
-                           inFilesByRegEx: #".*-resources\.sh"#,
-                           folder: supportFilesFolder)
+        let originalDirs = ["PODS_CONFIGURATION_BUILD_DIR", "BUILT_PRODUCTS_DIR"].joined(separator: "|")
+        let suffixPods = buildedProducts.joined(separator: "|")
+        let fileRegex = [#".*-resources\.sh"#, #".*\.xcconfig"#, #".*-frameworks\.sh"#].joined(separator: "|")
+        try replace(#"\$\{(\#(originalDirs))\}(?=\/(\#(suffixPods))\b)"#,
+                    with: cacheFolder,
+                    inFilesByRegEx: "(\(fileRegex))",
+                    folder: supportFilesFolder)
     }
 
     /// Replacing content of each file by regex criteria in selected folder.
-    private func replaceContent(occurrences: [(lookup: String, replace: String)],
-                                inFilesByRegEx fileRegEx: String,
-                                folder: Folder) throws {
+    private func replace(_ lookup: String,
+                         with replace: String,
+                         inFilesByRegEx fileRegEx: String,
+                         folder: Folder) throws {
         let regex = try RegEx(pattern: fileRegEx)
         for file in folder.files.recursive where regex.test(file.path) {
             var content = try file.readAsString()
-            for (lookup, replace) in occurrences {
-                content = content.replacingOccurrences(of: lookup, with: replace)
-            }
+            content = content.replacingOccurrences(of: lookup, with: replace, options: .regularExpression)
             try file.write(content)
         }
     }
