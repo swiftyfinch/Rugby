62bb9b28c202e2d2825a7194c9f00e8126820008
Vyacheslav Khorkov <artfintch@yandex.com>
2021-02-14 11:36:00 +0300

    Add version.

--- Sources/rugby/main.swift
+++ Sources/rugby/main.swift
@@ -2,6 +2,7 @@ import ArgumentParser
 
 struct Rugby: ParsableCommand {
     static var configuration = CommandConfiguration(
+        version: "0.0.3",
         subcommands: [Cache.self, Clean.self, Log.self],
         defaultSubcommand: Cache.self
     )
