1d96975bf2355759b81ad2df385370946a0af222
Vyacheslav Khorkov <artfintch@yandex.com>
2021-02-21 19:20:35 +0300

    Change main overview.

--- Sources/rugby/main.swift
+++ Sources/rugby/main.swift
@@ -2,7 +2,7 @@ import ArgumentParser
 
 struct Rugby: ParsableCommand {
     static var configuration = CommandConfiguration(
-        abstract: "🏈 Shake up pods project, build and throw away part of them.",
+        abstract: "🏈 Cache some pods: build and throw away part of them.".green,
         version: "0.0.6",
         subcommands: [Cache.self, Clean.self, Log.self],
         defaultSubcommand: Cache.self
