a0839fea7a9abd49fd350cf50db68403641c456a
Vyacheslav Khorkov <artfintch@yandex.com>
2021-02-21 16:45:55 +0300

    Add drop sources flag.

--- Sources/rugby/Common/Extensions/PBXProj/PBXProj+Target.swift
+++ Sources/rugby/Common/Extensions/PBXProj/PBXProj+Target.swift
@@ -24,9 +24,25 @@ extension PBXProj {
     @discardableResult
     func removeTarget(name: String) -> Bool {
         guard let target = main.targets.first(where: { $0.name == name }) else { return false }
-        target.dependencies.forEach(delete)
+
+        for phase in target.buildPhases {
+            for file in phase.files ?? [] {
+                delete(object: file)
+            }
+            delete(object: phase)
+        }
+
+        for configuration in target.buildConfigurationList?.buildConfigurations ?? [] {
+            delete(object: configuration)
+        }
+        target.buildConfigurationList.map(delete)
+
         target.dependencies.forEach { $0.targetProxy.map(delete) }
+        target.dependencies.forEach(delete)
+
         delete(object: target)
+        main.targets.removeAll(where: { $0.name == name })
+
         return true
     }
 }
