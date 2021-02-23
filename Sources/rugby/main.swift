890ee1307331038cd0083445ffab9e42f9af491f
Vyacheslav Khorkov <artfintch@yandex.com>
2021-02-23 17:57:59 +0300

    Bump version 0.1.0.

--- Sources/rugby/main.swift
+++ Sources/rugby/main.swift
@@ -3,7 +3,7 @@ import ArgumentParser
 struct Rugby: ParsableCommand {
     static var configuration = CommandConfiguration(
         abstract: "🏈 Cache some pods: build and throw away part of them.".green,
-        version: "0.0.6",
+        version: "0.1.0",
         subcommands: [Cache.self, Clean.self, Log.self],
         defaultSubcommand: Cache.self
     )
