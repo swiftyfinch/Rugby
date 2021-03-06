f1f49e36bef24e07704036f10dd8901358edd745
Vyacheslav Khorkov <artfintch@yandex.com>
2021-03-06 08:19:57 +0300

    Refactor cache integration step.

--- Sources/rugby/Commands/Cache/Integration/CacheIntegration.swift
+++ Sources/rugby/Commands/Cache/Integration/CacheIntegration.swift
@@ -17,22 +17,9 @@ struct CacheIntegration {
         let originalDirs = ["PODS_CONFIGURATION_BUILD_DIR", "BUILT_PRODUCTS_DIR"].joined(separator: "|")
         let suffixPods = buildedProducts.map { $0.escapeForRegex() }.joined(separator: "|")
         let fileRegex = [#".*-resources\.sh"#, #".*\.xcconfig"#, #".*-frameworks\.sh"#].joined(separator: "|")
-        try replace(#"\$\{(\#(originalDirs))\}(?=\/(\#(suffixPods))\b)"#,
-                    with: cacheFolder,
-                    inFilesByRegEx: "(\(fileRegex))",
-                    folder: supportFilesFolder)
-    }
-
-    /// Replacing content of each file by regex criteria in selected folder.
-    private func replace(_ lookup: String,
-                         with replace: String,
-                         inFilesByRegEx fileRegEx: String,
-                         folder: Folder) throws {
-        let regex = try RegEx(pattern: fileRegEx)
-        for file in folder.files.recursive where regex.test(file.path) {
-            var content = try file.readAsString()
-            content = content.replacingOccurrences(of: lookup, with: replace, options: .regularExpression)
-            try file.write(content)
-        }
+        try FileContentReplacer().replace(#"\$\{(\#(originalDirs))\}(?=\/(\#(suffixPods))\b)"#,
+                                          with: cacheFolder,
+                                          inFilesByRegEx: "(\(fileRegex))",
+                                          folder: supportFilesFolder)
     }
 }
