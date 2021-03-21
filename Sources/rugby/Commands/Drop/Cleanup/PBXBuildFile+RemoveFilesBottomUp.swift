2f316903c8892a1a40b2407d6a2f1fe456124213
Vyacheslav Khorkov <artfintch@yandex.com>
2021-03-21 16:57:37 +0300

    Rename external argument to projectPath.

--- Sources/rugby/Commands/Drop/Cleanup/PBXBuildFile+RemoveFilesBottomUp.swift
+++ Sources/rugby/Commands/Drop/Cleanup/PBXBuildFile+RemoveFilesBottomUp.swift
@@ -8,8 +8,10 @@
 import XcodeProj
 
 extension Array where Element == PBXBuildFile {
-    func removeFilesBottomUp(project: PBXProj) throws {
+    func removeFilesBottomUp(project: PBXProj, excludeFiles: Set<String>) throws {
         forEach { buildFile in
+            guard let uuid = buildFile.file?.uuid else { return }
+            if excludeFiles.contains(uuid) { return }
             let group = buildFile.file?.parent as? PBXGroup
             buildFile.file.map(project.delete)
             group?.removeIfEmpty(project: project, applyForParent: true)
