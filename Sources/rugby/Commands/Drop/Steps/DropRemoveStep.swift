9d5faf42b3c246f4275e19dd2de92a9adf703181
Vyacheslav Khorkov <artfintch@yandex.com>
2021-03-21 16:56:45 +0300

    Add recursively remove sources.

--- Sources/rugby/Commands/Drop/Steps/DropRemoveStep.swift
+++ Sources/rugby/Commands/Drop/Steps/DropRemoveStep.swift
@@ -48,9 +48,54 @@ final class DropRemoveStep: Step {
         done()
     }
 
-    private func removeSources(project: PBXProj, fromTarget name: String) throws {
+    // Need to verify if some files for removing depends on remaining targets
+    private func findFilesRemainingTargets(project: PBXProj, targetsForRemove: Set<String>) throws -> Set<String> {
+        func collectFiles(targets: [PBXTarget]) throws -> Set<String> {
+            try targets.reduce(into: []) { set, target in
+                for file in try target.sourcesBuildPhase()?.files ?? [] {
+                    guard let uuid = file.file?.uuid else { continue }
+                    set.insert(uuid)
+                }
+                for file in try target.resourcesBuildPhase()?.files ?? [] {
+                    guard let uuid = file.file?.uuid else { continue }
+                    set.insert(uuid)
+                }
+            }
+        }
+
+        let allTargets = project.main.targets
+        let remainingTargets = allTargets.filter { targetsForRemove.contains($0.name) }
+        let remainingFiles = try collectFiles(targets: remainingTargets)
+        let removingTargets = allTargets.filter { !targetsForRemove.contains($0.name) }
+        let removingFiles = try collectFiles(targets: removingTargets)
+        return remainingFiles.intersection(removingFiles)
+    }
+
+    private func removeSources(project: PBXProj, fromTarget name: String, excludeFiles: Set<String>) throws {
         guard let target = project.targets(named: name).first else { return }
-        try target.sourcesBuildPhase()?.files?.removeFilesBottomUp(project: project)
-        try target.resourcesBuildPhase()?.files?.removeFilesBottomUp(project: project)
+        try target.sourcesBuildPhase()?.files?.removeFilesBottomUp(project: project, excludeFiles: excludeFiles)
+        try target.resourcesBuildPhase()?.files?.removeFilesBottomUp(project: project, excludeFiles: excludeFiles)
+    }
+
+    @discardableResult
+    private func removeFrameworkPaths(project: PBXProj, groups: Set<String>, products: Set<String>) -> Bool {
+        var hasChanges = false
+        let frameworks = project.groups.filter {
+            ($0.name.map(groups.contains) ?? false) && $0.parent?.parent == nil
+        }
+        frameworks.forEach {
+            $0.children.forEach { child in
+                if let name = child.name, products.contains(name) {
+                    project.delete(object: child)
+                    (child.parent as? PBXGroup)?.children.removeAll(where: { child.uuid == $0.uuid })
+                    hasChanges = true
+                } else if let path = child.path, products.contains(path) {
+                    project.delete(object: child)
+                    (child.parent as? PBXGroup)?.children.removeAll(where: { child.uuid == $0.uuid })
+                    hasChanges = true
+                }
+            }
+        }
+        return hasChanges
     }
 }
