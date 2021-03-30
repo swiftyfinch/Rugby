a61ebd0e61b355cf349bb1bfc388ee9dd89b8047
Vyacheslav Khorkov <artfintch@yandex.com>
2021-03-30 14:51:18 +0300

    Drop test project.

--- Tests/TestProject/Scripts/exclude_arm64.rb
+++
@@ -1,7 +0,0 @@
-require 'Xcodeproj'
-
-project = Xcodeproj::Project.open("Pods/Pods.xcodeproj")
-project.build_configurations.each { |config|
-	project.build_settings(config.name)["EXCLUDED_ARCHS"] = "arm64"
-}
-project.save()
