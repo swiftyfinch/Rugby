8477162dc21c5158dfe60a592f087f033f50864d
Vyacheslav Khorkov <artfintch@yandex.com>
2021-04-03 21:51:17 +0300

    Update all packages.

--- Package.swift
+++ Package.swift
@@ -10,13 +10,13 @@ let package = Package(
         .executable(name: "rugby", targets: ["rugby"])
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.0.1"),
-        .package(url: "https://github.com/onevcat/Rainbow", from: "3.0.0"),
-        .package(url: "https://github.com/JohnSundell/Files", from: "4.0.0"),
-        .package(name: "XcodeProj", url: "https://github.com/tuist/xcodeproj", from: "7.18.0"),
-        .package(url: "https://github.com/eneko/RegEx.git", from: "0.1.0"),
-        .package(url: "https://github.com/JohnSundell/ShellOut.git", from: "2.0.0"),
-        .package(url: "https://github.com/realm/SwiftLint", from: "0.42.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "0.3.2"),
+        .package(url: "https://github.com/onevcat/Rainbow", from: "4.0.0"),
+        .package(url: "https://github.com/JohnSundell/Files", from: "4.2.0"),
+        .package(name: "XcodeProj", url: "https://github.com/tuist/xcodeproj", from: "7.20.0"),
+        .package(url: "https://github.com/eneko/RegEx.git", from: "0.3.0"),
+        .package(url: "https://github.com/JohnSundell/ShellOut.git", from: "2.3.0"),
+        .package(url: "https://github.com/realm/SwiftLint", from: "0.43.1"),
         .package(url: "https://github.com/jpsim/Yams.git", from: "4.0.5")
     ],
     targets: [
