29a0b03b784e7ede93f00d67ab32cec80160ac3a
Vyacheslav Khorkov <artfintch@yandex.com>
2021-04-05 22:13:21 +0300

    Clean up environment.

--- Sources/rugby/Common/Environment/String+Env.swift
+++ Sources/rugby/Common/Environment/String+Env.swift
@@ -6,21 +6,28 @@
 //
 
 extension String {
+
+    // MARK: - Output
+    static let finalMessage = "Let's roll 🏈".green
+
+    // MARK: - Common
+
+    static let podsGroup = "Pods"
+    static let supportFolder = ".rugby"
+    static let log = supportFolder + "/rugby.log"
     static let podfileLock = "Podfile.lock"
     static let podsProject = "Pods/Pods.xcodeproj"
     static let podsTargetSupportFiles = "Pods/Target Support Files"
 
-    static let supportFolder = ".rugby"
-    static let buildFolder = supportFolder + "/build"
-    static let log = supportFolder + "/rugby.log"
-    static let buildLog = supportFolder + "/build.log"
-    static let cachedChecksums = supportFolder + "/Checksums"
-    static let cacheFile = supportFolder + "/cache.yml"
-
-    static let podsGroup = "Pods"
+    // MARK: - Cache command
 
     static let defaultXcodeCLTPath = "/Library/Developer/CommandLineTools"
 
+    static let buildTarget = "RemotePods"
+    static let buildLog = supportFolder + "/build.log"
+    static let buildFolder = supportFolder + "/build"
+    static let cachedChecksums = supportFolder + "/Checksums"
+    static let cacheFile = supportFolder + "/cache.yml"
     static func cacheFolder(sdk: SDK) -> String {
         "${PODS_ROOT}/../" + supportFolder + "/build/Debug-\(sdk.xcodebuild)"
     }
