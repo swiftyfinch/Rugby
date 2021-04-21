374ccfb5f906db16b47855d9971a1476bd814121
Vyacheslav Khorkov <artfintch@yandex.com>
2021-04-21 20:42:02 +0300

    Remove timer from OneLinePrinter.

--- Sources/rugby/Common/LogPrinter/Printers/OneLinePrinter.swift
+++ Sources/rugby/Common/LogPrinter/Printers/OneLinePrinter.swift
@@ -8,50 +8,15 @@
 import Foundation
 
 final class OneLinePrinter {
-    private var queue: [String] = []
-    private var timer: Timer?
-    private var lastPrint: TimeInterval?
-
-    private static let dateFormatter: DateFormatter = {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "HH:mm:ss:SSS"
-        return dateFormatter
-    }()
-
-    private func setupTimer() -> Bool {
-        guard timer == nil else { return false }
-        Swift.print()
-        let timer = Timer(timeInterval: .printInterval, repeats: true, block: { [weak self] _ in
-            guard let self = self else { return }
-            guard !self.queue.isEmpty else { return }
-            Swift.print(self.queue.removeFirst())
-            self.lastPrint = ProcessInfo.processInfo.systemUptime
-        })
-        RunLoop.current.add(timer, forMode: .default)
-        self.timer = timer
-        return true
-    }
+    private var firstPrint = true
 }
 
 extension OneLinePrinter: Printer {
+
     func print(_ value: String) {
-        queue.append("\u{1B}[1A\u{1B}[K\(value)")
-        let begin = lastPrint ?? ProcessInfo.processInfo.systemUptime
-        let elapsed = ProcessInfo.processInfo.systemUptime - begin
-        if elapsed >= .printInterval {
-            timer?.fire()
-            return
-        }
-        if setupTimer() { timer?.fire() }
+        if firstPrint { Swift.print(); firstPrint = false }
+        Swift.print("\u{1B}[1A\u{1B}[K\(value)")
     }
 
-    func done() {
-        if timer == nil { Swift.print() }
-        timer?.invalidate()
-        queue.forEach { Swift.print($0) }
-    }
-}
-
-private extension TimeInterval {
-    static let printInterval: TimeInterval = 0.2
+    func done() {}
 }
