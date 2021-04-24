8a50569777d7d833973d457fe4af65857b214889
Vyacheslav Khorkov <artfintch@yandex.com>
2021-04-24 09:26:22 +0300

    Improve help message for navigate to github link.

--- Sources/rugby/main.swift
+++ Sources/rugby/main.swift
@@ -4,7 +4,7 @@ struct Rugby: ParsableCommand {
     static var configuration = CommandConfiguration(
         abstract: """
         \("🏈 Cache some pods: build and throw away part of them.".green)
-        📖 https://github.com/swiftyfinch/Rugby (⌘ + double click)
+        📖 https://github.com/swiftyfinch/Rugby (⌘ + double click on link)
         """,
         version: "1.4.0",
         subcommands: [Cache.self, Drop.self, Clean.self, Log.self],
