605d2d85d516ab2b2a239a40394cfb13c69dfa0a
Vyacheslav Khorkov <artfintch@yandex.com>
2021-04-24 09:41:24 +0300

    Change command help messages.

--- Sources/rugby/main.swift
+++ Sources/rugby/main.swift
@@ -3,8 +3,9 @@ import ArgumentParser
 struct Rugby: ParsableCommand {
     static var configuration = CommandConfiguration(
         abstract: """
-        \("🏈 Cache some pods: build and throw away part of them.".green)
-        📖 https://github.com/swiftyfinch/Rugby (⌘ + double click on link)
+
+        🏈 Shake up pods project, build and throw away part of it.
+        📖 \("https://github.com/swiftyfinch/Rugby".cyan) (⌘ + double click on link)
         """,
         version: "1.4.0",
         subcommands: [Cache.self, Drop.self, Clean.self, Log.self],
