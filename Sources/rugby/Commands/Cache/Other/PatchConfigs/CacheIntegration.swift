412fb1d43f006cb1b9de58327067c615e9039db3
Vyacheslav Khorkov <artfintch@yandex.com>
2021-05-02 17:43:57 +0300

    Make path replacing more accurate.

--- Sources/rugby/Commands/Cache/Other/PatchConfigs/CacheIntegration.swift
+++ Sources/rugby/Commands/Cache/Other/PatchConfigs/CacheIntegration.swift
@@ -17,9 +17,9 @@ struct CacheIntegration {
         let originalDirs = ["PODS_CONFIGURATION_BUILD_DIR", "BUILT_PRODUCTS_DIR"].joined(separator: "|")
         let suffixPods = buildedProducts.map { $0.escapeForRegex() }.joined(separator: "|")
         let fileRegex = [#".*-resources\.sh"#, #".*\.xcconfig"#, #".*-frameworks\.sh"#].joined(separator: "|")
-        try FilePatcher().replace(#"\$\{(\#(originalDirs))\}(?=\/(\#(suffixPods))\b)"#,
-                                          with: cacheFolder,
-                                          inFilesByRegEx: "(\(fileRegex))",
-                                          folder: supportFilesFolder)
+        try FilePatcher().replace(#"\$\{(\#(originalDirs))\}(?=\/(\#(suffixPods))("|\s|\/))"#,
+                                  with: cacheFolder,
+                                  inFilesByRegEx: "(\(fileRegex))",
+                                  folder: supportFilesFolder)
     }
 }
