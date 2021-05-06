929ce5334104e8b0dfe6152ebeaba1105cb8f730
Vyacheslav Khorkov <artfintch@yandex.com>
2021-05-06 20:25:16 +0300

    Add all available parameters to plans example.

--- Sources/rugby/Commands/Plan/Subcommands/PlansExample.swift
+++ Sources/rugby/Commands/Plan/Subcommands/PlansExample.swift
@@ -32,15 +32,36 @@ struct PlansExample: ParsableCommand {
         }
 
         let plansFile = try Folder.current.createFile(at: .plans)
-        try plansFile.write("""
+        try plansFile.write(Self.template)
+    }
+
+    private static let template: String = """
         # The first plan in file always run by default.
         - usual:
           # The first command without arguments like: rugby cache
           - command: cache
+            # Optional parameters:
+            skipParents: true
+            #arch: null
+            #sdk: sim
+            #keepSources: false
+            #exclude: []
+            #hideMetrics: false
+            #ignoreCache: false
+            #verbose: false
           # The second command: rugby drop "Test"
           - command: drop
             targets:
               - Test
+            # Optional parameters:
+            #targets: []
+            #invert: false
+            #project: "Pods/Pods.xcodeproj"
+            #testFlight: false
+            #keepSources: false
+            #exclude: []
+            #hideMetrics: false
+            #verbose: false
           # And so on: rugby drop -i "TestProject" -p TestProject/TestProject.xcodeproj
           - command: drop
             targets:
@@ -51,10 +72,9 @@ struct PlansExample: ParsableCommand {
         # Also, you can use custom plan: rugby --plan unit
         - unit:
           - command: cache
-            # Alternative syntax for yml arrays:
             exclude: [Alamofire]
           - command: drop
             targets: [Test]
-        """)
-    }
+            exclude: [MyFeatureTests]
+        """
 }
