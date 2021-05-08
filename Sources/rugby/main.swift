ea37fcc02ace2ce99f60a209391c51ff028b1c76
Vyacheslav Khorkov <artfintch@yandex.com>
2021-05-08 18:46:00 +0300

    Bump version 1.5.1.

--- Sources/rugby/main.swift
+++ Sources/rugby/main.swift
@@ -7,7 +7,7 @@ struct Rugby: ParsableCommand {
         🏈 Shake up pods project, build and throw away part of it.
         📖 \("https://github.com/swiftyfinch/Rugby".cyan) (⌘ + double click on link)
         """,
-        version: "1.5.0",
+        version: "1.5.1",
         subcommands: [Plans.self, Cache.self, Drop.self, Log.self, Clean.self],
         defaultSubcommand: Plans.self
     )
