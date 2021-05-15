f25976e238445ae7c3edf389df435874db5c32ab
Vyacheslav Khorkov <artfintch@yandex.com>
2021-05-15 15:09:23 +0300

    Add removing sources from development pods group.

--- Sources/rugby/Common/XcodeProj/Files/XcodeProj+RemoveSources.swift
+++ Sources/rugby/Common/XcodeProj/Files/XcodeProj+RemoveSources.swift
@@ -8,14 +8,14 @@
 import XcodeProj
 
 extension XcodeProj {
-    func removeSources(pods: Set<String>) -> Bool {
-        guard let podsGroup = pbxproj.groups.first(where: { $0.name == .podsGroup && $0.parent?.parent == nil }) else {
+    func removeSources(pods: Set<String>, fromGroup groupName: String) -> Bool {
+        guard let group = pbxproj.groups.first(where: { $0.name == groupName && $0.parent?.parent == nil }) else {
             return false
         }
-        removeFilesRecursively(fromFile: podsGroup, pods: pods)
-        if podsGroup.children.isEmpty {
-            pbxproj.delete(object: podsGroup)
-            (podsGroup.parent as? PBXGroup)?.children.removeAll { $0.name == .podsGroup }
+        removeFilesRecursively(fromFile: group, rootGroup: groupName, pods: pods)
+        if group.children.isEmpty {
+            pbxproj.delete(object: group)
+            (group.parent as? PBXGroup)?.children.removeAll { $0.name == groupName }
         }
         return true
     }
