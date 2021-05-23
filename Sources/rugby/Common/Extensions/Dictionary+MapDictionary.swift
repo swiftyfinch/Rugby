5dcad733aedf17608fe0b8a23fa61b3f3230301e
Vyacheslav Khorkov <artfintch@yandex.com>
2021-05-23 17:44:55 +0300

    Fix linter warning.

--- Sources/rugby/Common/Extensions/Dictionary+MapDictionary.swift
+++ Sources/rugby/Common/Extensions/Dictionary+MapDictionary.swift
@@ -9,7 +9,7 @@ extension Dictionary {
     /// Transforms dictionary to another one with different type keys and values.
     func mapDictionary<NewKey, NewValue>(
         transform: ((key: Key, value: Value)) throws -> (NewKey, NewValue)
-    ) rethrows -> Dictionary<NewKey, NewValue> {
+    ) rethrows -> [NewKey: NewValue] {
         .init(uniqueKeysWithValues: try map(transform))
     }
 }
