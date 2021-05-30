8e9ef31135ab3e8cfada3a44c534b69cfe88ed89
Vyacheslav Khorkov <artfintch@yandex.com>
2021-05-30 22:47:58 +0300

    Remove pods folder from git.

--- TestProject/Pods/Target Support Files/AutoMate/AutoMate-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_AutoMate : NSObject
-@end
-@implementation PodsDummy_AutoMate
-@end
--- TestProject/Pods/Target Support Files/Pods-TestProject/Pods-TestProject-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TestProject : NSObject
-@end
-@implementation PodsDummy_Pods_TestProject
-@end
