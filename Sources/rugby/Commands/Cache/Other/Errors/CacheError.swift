9b208f2b61851b50e8843a1b5a124aa373024f9e
Vyacheslav Khorkov <artfintch@yandex.com>
2021-06-03 21:14:47 +0300

    Simplify help of build failed error.

--- Sources/rugby/Commands/Cache/Other/Errors/CacheError.swift
+++ Sources/rugby/Commands/Cache/Other/Errors/CacheError.swift
@@ -23,7 +23,7 @@ enum CacheError: Error, LocalizedError {
             output = "Couldn't find Xcode CLT.\n".red
                 + "🚑 Check Xcode Preferences → Locations → Command Line Tools.".yellow
         case .buildFailed:
-            let buildCommand = "cat \(String.buildLog) | xcpretty --color | less -r +G"
+            let buildCommand = "cat \(String.buildLog) | xcpretty"
             output = "Build failed.\n".red
                 + "🚑 Run for more info: ".yellow + buildCommand.white
         }
