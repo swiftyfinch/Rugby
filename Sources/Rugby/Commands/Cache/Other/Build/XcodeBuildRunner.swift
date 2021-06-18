7d8db0f893be4973e35891c9dbdac49dc63d87bf
Vyacheslav Khorkov <artfintch@yandex.com>
2021-06-18 22:28:13 +0300

    Beautify log on error too

--- Sources/Rugby/Commands/Cache/Other/Build/XcodeBuildRunner.swift
+++ Sources/Rugby/Commands/Cache/Other/Build/XcodeBuildRunner.swift
@@ -6,22 +6,30 @@
 //
 
 import Files
+import SwiftShell
 
 final class XcodeBuildRunner {
+    let rawLogPath: String
     let logPath: String
 
-    init(logPath: String) {
+    init(rawLogPath: String, logPath: String) {
+        self.rawLogPath = rawLogPath
         self.logPath = logPath
     }
 
     func run(_ command: String, args: Any ...) throws {
+        _ = try? shell(command, args: args)
+        try? beautifyLog()
+    }
+
+    private func beautifyLog() throws {
         let log = try Folder.current.createFile(at: logPath)
         let logFormatter = LogFormatter { formattedLine in
             try log.append(formattedLine + "\n")
         }
 
-        let output = try shell(command, args: args)
-        for line in output.components(separatedBy: .newlines) {
+        let rawLog = try open(.rawBuildLog)
+        for line in rawLog.lines() {
             try logFormatter.format(line: line)
         }
         try logFormatter.finish()
