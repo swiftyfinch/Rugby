89abfe2efa57cb89720275757312279d070b2bff
Vyacheslav Khorkov <artfintch@yandex.com>
2021-07-27 18:10:59 +0300

    Change doctor output

--- Sources/Rugby/Commands/Doctor/Doctor.swift
+++ Sources/Rugby/Commands/Doctor/Doctor.swift
@@ -13,19 +13,20 @@ struct Doctor: ParsableCommand {
         abstract: "• Show troubleshooting suggestions."
     )
 
-    func run() throws {
-        let output = """
-        🚑 If you encourage any problems, please follow this checklist:
-        1. Firstly, update Rugby to the last version;
-        2. Run \("rugby --ignore-checksums".yellow);
-        3. Try to investigate build logs yourself;
-        4. Run \("rugby clean && rugby --ignore-checksums".yellow);
-        5. Check that Pods project builds successfully without Rugby.
+    private static let output = """
+    🚑 \("Doctor:".green) If you encourage any problems, please follow this checklist:
+    1. Firstly, update Rugby to the last version;
+    2. Run \("rugby --ignore-checksums".yellow);
+    3. Try to investigate build logs yourself;
+    4. Run \("rugby clean && rugby --ignore-checksums".yellow);
+    5. Check that Pods project builds successfully without Rugby.
 
-        Report an issue in GitHub discussions or any convenience support channel.
-        Attach last files from \(".rugby/history".yellow) folder.
-        But be sure that there are \("no sensitive".red) data.
-        """
-        print(output)
+    Report an issue in GitHub discussions or any convenience support channel.
+    Attach last files from \(".rugby/history".yellow) folder.
+    But be sure that there are \("no sensitive".red) data.
+    """
+
+    func run() throws {
+        print(Doctor.output)
     }
 }
