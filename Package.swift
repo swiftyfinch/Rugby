64fa8f590f1331cfd95e69d21c9a58abf7f21b77
Muukii <muukii.app@gmail.com>
2021-09-24 18:40:19 +0900

    Update packages

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         .package(url: "https://github.com/apple/swift-argument-parser", from: "0.4.3"),
         .package(url: "https://github.com/onevcat/Rainbow", from: "4.0.0"),
         .package(url: "https://github.com/JohnSundell/Files", from: "4.2.0"),
-        .package(name: "XcodeProj", url: "https://github.com/tuist/xcodeproj", from: "7.22.0"),
+        .package(name: "XcodeProj", url: "https://github.com/tuist/xcodeproj", from: "8.3.1"),
         .package(url: "https://github.com/kareman/SwiftShell", from: "5.1.0"),
         .package(url: "https://github.com/jpsim/Yams.git", from: "4.0.6"),
 
