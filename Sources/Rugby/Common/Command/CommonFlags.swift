ec3a3c05be468b2abb943fb97a6a81fe5a5ed9e5
Vyacheslav Khorkov <artfintch@yandex.com>
2021-10-23 18:54:12 +0300

    Add common flags command

---
+++ Sources/Rugby/Common/Command/CommonFlags.swift
@@ -0,0 +1,15 @@
+//
+//  CommonFlags.swift
+//  Rugby
+//
+//  Created by Vyacheslav Khorkov on 23.10.2021.
+//  Copyright © 2021 Vyacheslav Khorkov. All rights reserved.
+//
+
+import ArgumentParser
+
+struct CommonFlags: ParsableCommand {
+    @Flag(name: .long, inversion: .prefixedNo, help: "Play bell sound on finish.") var bell = true
+    @Flag(help: "Hide metrics.") var hideMetrics = false
+    @Flag(name: .shortAndLong, help: "Print more information.") var verbose: Int
+}
