cc8c99370eb023794f30ff661ccf45e553510269
Vyacheslav Khorkov <swiftyfinch@yandex.com>
2021-11-20 21:58:48 +0300

    Add job for spell checking

--- Scripts/checkSpell.sh
+++
@@ -1,4 +0,0 @@
-# It's a temporary solution to check the spelling of Docs/*.md and Sources/Rugby/*.swift files
-# Need to install https://github.com/fromkk/SpellChecker
-find Docs -type f -name '*.md' -exec SpellChecker --yml Scripts/spell_checker_whitelist.yml -- {} \; | grep -v 'no typo'
-find Sources/Rugby -type f -name '*.swift' -exec SpellChecker --yml Scripts/spell_checker_whitelist.yml -- {} \; | grep -v 'no typo'
