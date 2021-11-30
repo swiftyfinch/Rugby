f0c3b1569b801168c1502d03cd1cc418d875ce4f
Vyacheslav Khorkov <swiftyfinch@yandex.com>
2021-11-30 21:03:29 +0300

    Update doctor

--- Sources/Rugby/Commands/Doctor/Doctor.swift
+++ Sources/Rugby/Commands/Doctor/Doctor.swift
@@ -14,12 +14,22 @@ struct Doctor: ParsableCommand {
     )
 
     private static let output = """
-    🚑 \("Doctor:".green) If you encourage any problems, please follow this checklist:
-    1. Firstly, update Rugby to the last version;
-    2. Run \("rugby --ignore-checksums".yellow);
-    3. Try to investigate build logs yourself;
-    4. Run \("rugby clean && rugby --ignore-checksums".yellow);
+    🚑 If you have any problems, please try these solutions.
+    After each one, you need to repeat your usual workflow.
+
+    1. Update 🏈 Rugby to the last version:
+       \("mint install swiftyfinch/rugby".yellow)
+    2. Add ignore option \("rugby --ignore-checksums".yellow).
+       Be careful, you can't pass this option to plans command this way;
+    3. Try \("rugby clean".yellow) before repeating your usual workflow;
+    4. Try to clean up your \("DerivedData".yellow) and use \("rugby clean".yellow) together;
     5. Check that Pods project builds successfully without Rugby.
+
+    By the way, you can try to investigate build logs by yourself:
+    \(".rugby/rugby.log".yellow)
+    \(".rugby/build.log".yellow)
+    \(".rugby/rawBuild.log".yellow)
+    and more there \(".rugby/history".yellow)
 
     Report an issue in GitHub discussions or any convenience support channel.
     Attach last files from \(".rugby/history".yellow) folder.
