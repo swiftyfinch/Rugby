54a41dc6b3ec8dca6a1d0c0e5fb6aa4e403dc1c6
shreesha-kedilaya <shreeshakedilaya@gmail.com>
2022-05-16 19:10:40 +0530

    Added back files

---
+++ Flutter_test_app/FlutterRugbyIntegration/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FlutterRugbyIntegration
+//
+//  Created by Shreesha on 15/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
