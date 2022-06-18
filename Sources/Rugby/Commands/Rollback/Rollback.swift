730bd9cbf07cef45f92da781790b5dda9d7c94a2
Vyacheslav Khorkov <swiftyfinch@yandex.com>
2022-06-18 13:24:04 +0500

    Implement rollback command

---
+++ Sources/Rugby/Commands/Rollback/Rollback.swift
@@ -0,0 +1,36 @@
+//
+//  Rollback.swift
+//  Rugby
+//
+//  Created by Vyacheslav Khorkov on 16.06.2022.
+//  Copyright © 2021 Vyacheslav Khorkov. All rights reserved.
+//
+
+import ArgumentParser
+import Files
+
+struct Rollback: ParsableCommand, Command {
+    @Flag(name: .shortAndLong, help: "Print more information.") var verbose: Int
+    @Flag(name: .shortAndLong, help: "Print nothing.") var quiet = false
+
+    static var configuration = CommandConfiguration(
+        abstract: "• \("(Beta)".yellow) Deintegrate Rugby from your project.",
+        shouldDisplay: false
+    )
+
+    mutating func run() throws {
+        try WrappedError.wrap(playBell: false) {
+            try wrappedRun()
+        }
+    }
+}
+
+extension Rollback {
+    mutating func run(logFile: File) throws -> Metrics? {
+        let progress = RugbyPrinter(title: "Rollback", logFile: logFile, verbose: verbose, quiet: quiet)
+        let backupManager = BackupManager(progress: progress)
+        try backupManager.rollback()
+        progress.done()
+        return nil
+    }
+}
