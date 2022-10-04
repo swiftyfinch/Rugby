36890d608c3b04094abc9748f644d674b4bee4df
Vyacheslav Khorkov <swiftyfinch@yandex.com>
2022-10-04 20:57:37 +0500

    Use CryptoKit instead of CommonCrypto

--- Sources/Rugby/Common/Extensions/Crypto/Data+SHA1.swift
+++ Sources/Rugby/Common/Extensions/Crypto/Data+SHA1.swift
@@ -5,14 +5,13 @@
 //  Created by Colton Schlosser on 03.08.2022.
 //
 
-import CommonCrypto
+import CryptoKit
 import Foundation
 
 extension Data {
     func sha1() -> String {
-        var digest = [UInt8](repeating: 0, count: Int(CC_SHA1_DIGEST_LENGTH))
-        withUnsafeBytes { _ = CC_SHA1($0.baseAddress, CC_LONG(count), &digest) }
-        let hexBytes = digest.map { String(format: "%02hhx", $0) }
-        return hexBytes.joined()
+        Insecure.SHA1.hash(data: self).map {
+            String(format: "%02hhx", $0)
+        }.joined()
     }
 }
