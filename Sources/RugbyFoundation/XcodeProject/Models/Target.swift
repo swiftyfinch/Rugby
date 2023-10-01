29473e228ed3eaaec1695ab26cfa723b7cf91258
Vyacheslav Khorkov <swiftyfinch@yandex.com>
2023-10-01 15:15:56 +0500

    Format remaining files after experiments

--- Sources/RugbyFoundation/XcodeProject/Models/Target.swift
+++ Sources/RugbyFoundation/XcodeProject/Models/Target.swift
@@ -1,11 +1,3 @@
-//
-//  Target.swift
-//  RugbyFoundation
-//
-//  Created by Vyacheslav Khorkov on 27.07.2022.
-//  Copyright © 2022 Vyacheslav Khorkov. All rights reserved.
-//
-
 import Foundation
 import XcodeProj
 
