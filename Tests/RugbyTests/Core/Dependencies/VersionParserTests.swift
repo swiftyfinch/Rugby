14607341ee3bb3429bfec1e8e979fd870d614505
Marcel Canhisares <marcel.canhisares@gmail.com>
2023-10-01 14:32:19 -0400

    fix: Linting test files

--- Tests/RugbyTests/Core/Dependencies/VersionParserTests.swift
+++ Tests/RugbyTests/Core/Dependencies/VersionParserTests.swift
@@ -1,6 +1,6 @@
 //
 //  VersionParserTests.swift
-//  
+//
 //
 //  Created by Marcel Canhisares on 2023-09-30.
 //
@@ -9,12 +9,12 @@ import XCTest
 
 final class VersionParserTests: XCTestCase {
     private var parser: VersionParser!
-    
+
     override func setUp() {
         super.setUp()
         parser = VersionParser()
     }
-    
+
     override func tearDown() {
         super.tearDown()
         parser = nil
@@ -22,12 +22,12 @@ final class VersionParserTests: XCTestCase {
 }
 
 extension VersionParserTests {
-
     func testParseValidVersion() throws {
         let version = "1.0.0"
-        let ghVersion = GitHubUpdaterVersion(major: 1, minor: 0, patch: 0, prerelease: GitHubUpdaterVersion.Prerelease.prod, build: 0)        
+        let ghVersion = GitHubUpdaterVersion(major: 1, minor: 0, patch: 0,
+                                             prerelease: GitHubUpdaterVersion.Prerelease.prod, build: 0)
         // Act
-        let parsedVersion = try self.parser.parse(version)
+        let parsedVersion = try parser.parse(version)
 
         // Assert
         XCTAssertEqual(ghVersion.major, parsedVersion.major)
@@ -36,12 +36,13 @@ extension VersionParserTests {
         XCTAssertEqual(ghVersion.prerelease, parsedVersion.prerelease)
         XCTAssertEqual(ghVersion.build, parsedVersion.build)
     }
-    
+
     func testParseValidBetaVersion() throws {
         let version = "1.0.0b"
-        let ghVersion = GitHubUpdaterVersion(major: 1, minor: 0, patch: 0, prerelease: GitHubUpdaterVersion.Prerelease.beta, build: 0)
+        let ghVersion = GitHubUpdaterVersion(major: 1, minor: 0, patch: 0,
+                                             prerelease: GitHubUpdaterVersion.Prerelease.beta, build: 0)
         // Act
-        let parsedVersion = try self.parser.parse(version)
+        let parsedVersion = try parser.parse(version)
 
         // Assert
         XCTAssertEqual(ghVersion.major, parsedVersion.major)
@@ -50,13 +51,14 @@ extension VersionParserTests {
         XCTAssertEqual(ghVersion.prerelease, parsedVersion.prerelease)
         XCTAssertEqual(ghVersion.build, parsedVersion.build)
     }
-    
+
     func testParseValidBuildVersion() throws {
         let version = "1.0.0b1"
-        let ghVersion = GitHubUpdaterVersion(major: 1, minor: 0, patch: 0, prerelease: GitHubUpdaterVersion.Prerelease.beta, build: 1)
+        let ghVersion = GitHubUpdaterVersion(major: 1, minor: 0, patch: 0,
+                                             prerelease: GitHubUpdaterVersion.Prerelease.beta, build: 1)
         // Act
-        let parsedVersion = try self.parser.parse(version)
-        
+        let parsedVersion = try parser.parse(version)
+
         // Assert
         XCTAssertEqual(ghVersion.major, parsedVersion.major)
         XCTAssertEqual(ghVersion.minor, parsedVersion.minor)
@@ -64,13 +66,14 @@ extension VersionParserTests {
         XCTAssertEqual(ghVersion.prerelease, parsedVersion.prerelease)
         XCTAssertEqual(ghVersion.build, parsedVersion.build)
     }
-    
+
     func testParseInvalidBuildVersion() throws {
         let version = "1.0.0ba"
-        let ghVersion = GitHubUpdaterVersion(major: 1, minor: 0, patch: 0, prerelease: GitHubUpdaterVersion.Prerelease.beta, build: 1)
+        let ghVersion = GitHubUpdaterVersion(major: 1, minor: 0, patch: 0,
+                                             prerelease: GitHubUpdaterVersion.Prerelease.beta, build: 1)
         // Act
-        let parsedVersion = try self.parser.parse(version)
-        
+        let parsedVersion = try parser.parse(version)
+
         // Assert
         XCTAssertEqual(ghVersion.major, parsedVersion.major)
         XCTAssertEqual(ghVersion.minor, parsedVersion.minor)
@@ -78,13 +81,14 @@ extension VersionParserTests {
         XCTAssertEqual(ghVersion.prerelease, parsedVersion.prerelease)
         XCTAssertNotEqual(ghVersion.build, parsedVersion.build)
     }
-    
+
     func testParseInvalidBetaVersion() throws {
         let version = "1.0.0a1"
-        let ghVersion = GitHubUpdaterVersion(major: 1, minor: 0, patch: 0, prerelease: GitHubUpdaterVersion.Prerelease.beta, build: 1)
+        let ghVersion = GitHubUpdaterVersion(major: 1, minor: 0, patch: 0,
+                                             prerelease: GitHubUpdaterVersion.Prerelease.beta, build: 1)
         // Act
-        let parsedVersion = try self.parser.parse(version)
-        
+        let parsedVersion = try parser.parse(version)
+
         // Assert
         XCTAssertEqual(ghVersion.major, parsedVersion.major)
         XCTAssertEqual(ghVersion.minor, parsedVersion.minor)
@@ -92,21 +96,21 @@ extension VersionParserTests {
         XCTAssertNotEqual(ghVersion.prerelease, parsedVersion.prerelease)
         XCTAssertNotEqual(ghVersion.build, parsedVersion.build)
     }
-    
+
     func testInvalidShortVersionParse() {
         let version = "1.0"
         let expectedLocalizedDescription = VersionParserError.incorrectVersion(version).localizedDescription
-                
+
         // Act & Assert
         XCTAssertThrowsError(try parser.parse(version)) { error in
             XCTAssertEqual((error as? VersionParserError)?.localizedDescription, expectedLocalizedDescription)
         }
     }
-    
+
     func testInvalidCharVersionParse() {
         let version = "1.o.0"
         let expectedLocalizedDescription = VersionParserError.incorrectVersion(version).localizedDescription
-                
+
         // Act & Assert
         XCTAssertThrowsError(try parser.parse(version)) { error in
             XCTAssertEqual((error as? VersionParserError)?.localizedDescription, expectedLocalizedDescription)
