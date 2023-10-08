469f2e0f3d8d4002e1bda47edc572c5ffcd8c5b5
rock88 <rock88a@gmail.com>
2023-10-08 14:59:20 +0300

    Added tests for pod/unit_test resource bundles

---
+++ Example/LocalPods/LocalPod/Tests/ResourceBundleTests.swift
@@ -0,0 +1,23 @@
+import XCTest
+@testable import LocalPod
+
+final class ResourceBundleTests: XCTestCase {
+    func testAccessToResourcesBundle() throws {
+        try dummyTest(.resourcesBundle, expectedValue: "another")
+    }
+
+    func testAccessToTestResourcesBundle() throws {
+        try dummyTest(.testResourcesBundle, expectedValue: "some")
+    }
+
+    private func dummyTest(_ bundle: Bundle, expectedValue: String,  file: StaticString = #filePath, line: UInt = #line) throws {
+        let path = bundle.path(forResource: "dummy", ofType: "json") ?? ""
+        let data = try Data(contentsOf: URL(filePath: path))
+        let dummy = try JSONDecoder().decode(Dummy.self, from: data)
+        XCTAssertEqual(dummy.id, expectedValue, file: file, line: line)
+    }
+}
+
+struct Dummy: Codable {
+    let id: String
+}
