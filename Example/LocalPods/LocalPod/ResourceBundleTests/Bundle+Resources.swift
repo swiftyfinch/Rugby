d141c61af6326573663bfe792cc9b9d9d1dfc3af
Vyacheslav Khorkov <swiftyfinch@yandex.com>
2023-10-09 21:58:59 +0500

    Add separate resource bundle tests

--- Example/LocalPods/LocalPod/ResourceBundleTests/Bundle+Resources.swift
+++ Example/LocalPods/LocalPod/ResourceBundleTests/Bundle+Resources.swift
@@ -3,9 +3,9 @@ import Foundation
 private final class BundleToken {}
 
 extension Bundle {
-    static let resourcesBundle: Bundle! = {
+    static let testResourcesBundle: Bundle! = {
         let currentBundle = Bundle(for: BundleToken.self)
-        let resourcesBundleName = "LocalPodResources"
+        let resourcesBundleName = "LocalPodResourceBundleTestsResources"
         let resourcesBundlePath: String! = currentBundle.path(forResource: resourcesBundleName, ofType: "bundle")
         return Bundle(path: resourcesBundlePath)
     }()
