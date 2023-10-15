f7bf616d629b41229be48e6355c2fcd9be104fe8
Vyacheslav Khorkov <swiftyfinch@yandex.com>
2023-10-15 19:17:15 +0500

    Move BuildError to separate file

--- Sources/RugbyFoundation/Core/Build/BuildManager.swift
+++ Sources/RugbyFoundation/Core/Build/BuildManager.swift
@@ -18,27 +18,6 @@ public protocol IBuildManager {
                ignoreCache: Bool) async throws
 }
 
-enum BuildError: LocalizedError {
-    case buildFailed(errors: [String], buildLogPath: String, rawBuildLogPath: String)
-    case cantFindBuildTargets
-
-    var errorDescription: String? {
-        switch self {
-        case let .buildFailed(errors, buildLogPath, rawBuildLogPath):
-            return """
-            \("Build failed.".red)
-
-            \(errors.joined(separator: "\n").white)
-            \("🚑 More information in build logs:".yellow)
-            \("[Beautified]".yellow) \("cat \(buildLogPath.homeFinderRelativePath())".white)
-            \("[Raw]".yellow) \("open \(rawBuildLogPath.homeFinderRelativePath())".white)
-            """
-        case .cantFindBuildTargets:
-            return "Couldn't find any build targets."
-        }
-    }
-}
-
 // MARK: - Implementation
 
 final class BuildManager: Loggable {
