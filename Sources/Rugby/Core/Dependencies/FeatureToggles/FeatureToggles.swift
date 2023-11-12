fc0c2a4d0106585e038b15dd143c3e96d444e831
Vyacheslav Khorkov <swiftyfinch@yandex.com>
2023-11-12 15:02:16 +0500

    Handle different values in rugby environment variables

--- Sources/Rugby/Core/Dependencies/FeatureToggles/FeatureToggles.swift
+++ Sources/Rugby/Core/Dependencies/FeatureToggles/FeatureToggles.swift
@@ -2,11 +2,25 @@ import Foundation
 import RugbyFoundation
 
 final class FeatureToggles: IFeatureToggles {
+    private let RUGBY_KEEP_HASH_YAMLS = "RUGBY_KEEP_HASH_YAMLS"
+    private let RUGBY_PRINT_MISSING_BINARIES = "RUGBY_PRINT_MISSING_BINARIES"
+
     var keepHashYamls: Bool {
-        ProcessInfo.processInfo.environment["RUGBY_KEEP_HASH_YAMLS"] != nil
+        ProcessInfo.processInfo.environment[RUGBY_KEEP_HASH_YAMLS].isEnabled
     }
 
     var printMissingBinaries: Bool {
-        ProcessInfo.processInfo.environment["RUGBY_PRINT_MISSING_BINARIES"] != nil
+        ProcessInfo.processInfo.environment[RUGBY_PRINT_MISSING_BINARIES].isEnabled
+    }
+}
+
+private extension Optional where Wrapped == String {
+    var isEnabled: Bool {
+        switch self {
+        case "YES", "true", "1":
+            return true
+        default:
+            return false
+        }
     }
 }
