4689d056731107c903ae488d1a7d966fcb6d78c9
Vyacheslav Khorkov <swiftyfinch@yandex.com>
2023-11-12 15:03:01 +0500

    Describe all feature toggle as env dictionary

--- Sources/Rugby/Core/Dependencies/FeatureToggles/FeatureToggles.swift
+++ Sources/Rugby/Core/Dependencies/FeatureToggles/FeatureToggles.swift
@@ -1,9 +1,19 @@
 import Foundation
 import RugbyFoundation
 
-final class FeatureToggles: IFeatureToggles {
+final class FeatureToggles {
     private let RUGBY_KEEP_HASH_YAMLS = "RUGBY_KEEP_HASH_YAMLS"
     private let RUGBY_PRINT_MISSING_BINARIES = "RUGBY_PRINT_MISSING_BINARIES"
+
+}
+
+extension FeatureToggles: IFeatureToggles {
+    var all: [String: String] {
+        [
+            RUGBY_KEEP_HASH_YAMLS: keepHashYamls.yesNo,
+            RUGBY_PRINT_MISSING_BINARIES: printMissingBinaries.yesNo
+        ]
+    }
 
     var keepHashYamls: Bool {
         ProcessInfo.processInfo.environment[RUGBY_KEEP_HASH_YAMLS].isEnabled
@@ -23,4 +33,8 @@ private extension Optional where Wrapped == String {
             return false
         }
     }
+}
+
+private extension Bool {
+    var yesNo: String { self ? "YES" : "NO" }
 }
--- Sources/RugbyFoundation/Core/Common/IFeatureToggles.swift
+++ Sources/RugbyFoundation/Core/Common/IFeatureToggles.swift
@@ -1,5 +1,8 @@
 /// The protocol describing a service providing the feature toggles.
 public protocol IFeatureToggles: AnyObject {
+    /// The feature names and their values.
+    var all: [String: String] { get }
+
     /// A flag to keep yaml files with target hash in the binaries folder.
     var keepHashYamls: Bool { get }
 
