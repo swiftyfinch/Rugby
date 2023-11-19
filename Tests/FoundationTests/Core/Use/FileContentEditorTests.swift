3a0ca3bc0334a3ecb13d394c13ce4755b357a13f
Yunl Huang <cragodn@gmail.com>
2023-11-20 01:50:24 +0800

    Updated the regex for boundary matching

--- Tests/FoundationTests/Core/Use/FileContentEditorTests.swift
+++ Tests/FoundationTests/Core/Use/FileContentEditorTests.swift
@@ -42,7 +42,9 @@ extension FileContentEditorTests {
             "${PODS_CONFIGURATION_BUILD_DIR}/Moya/Moya.framework/Headers":
                 "${HOME}/.rugby/bin/Moya/${CONFIGURATION}${EFFECTIVE_PLATFORM_NAME}-${ARCHS}/badaa58/Moya.framework/Headers"
         ]
-        let regexPattern = try #"(\$\{PODS_CONFIGURATION_BUILD_DIR\}/Realm-library|\$\{PODS_CONFIGURATION_BUILD_DIR\}/Realm-library/libRealm-library\.a/Headers|\$\{PODS_CONFIGURATION_BUILD_DIR\}/Moya/Moya\.framework/Headers)\b"#.regex()
+        let joinedReplacementStr = #"(\$\{PODS_CONFIGURATION_BUILD_DIR\}/Realm-library|\$\{PODS_CONFIGURATION_BUILD_DIR\}/Realm-library/libRealm-library\.a/Headers|\$\{PODS_CONFIGURATION_BUILD_DIR\}/Moya/Moya\.framework/Headers)"#
+        // Ensured that the validation method here matches the matching approach in SupportFilesPatcher.
+        let regexPattern = try "\(ReplacementBoundary.prefix)\(joinedReplacementStr)\(ReplacementBoundary.suffix)".regex()
         let expectedNewContent = """
         FRAMEWORK_SEARCH_PATH = $(Inherited) "${HOME}/.rugby/bin/Realm-library/${CONFIGURATION}${EFFECTIVE_PLATFORM_NAME}-${ARCHS}/d2d48c5"
         HEADER_SEARCH_PATH = $(Inherited) ${HOME}/.rugby/bin/Realm-library/${CONFIGURATION}${EFFECTIVE_PLATFORM_NAME}-${ARCHS}/d2d48c5/libRealm-library.a/Headers '${HOME}/.rugby/bin/Moya/${CONFIGURATION}${EFFECTIVE_PLATFORM_NAME}-${ARCHS}/badaa58/Moya.framework/Headers'
