f897baafc209ebce566c725bad9c4d6d880d281d
Yunl Huang <cragodn@gmail.com>
2023-11-19 19:17:12 +0800

    Fix lint error

--- Sources/RugbyFoundation/Core/Use/FileContentEditor.swift
+++ Sources/RugbyFoundation/Core/Use/FileContentEditor.swift
@@ -2,33 +2,32 @@ import Fish
 import Foundation
 
 final class FileContentEditor {
-
     func replace(_ replacements: [String: String], regex: NSRegularExpression, filePath: String) throws {
         guard File.isExist(at: filePath) else { return }
         let file = try File.at(filePath)
         try replace(replacements, regex: regex, file: file)
     }
-    
+
     func replace(_ replacements: [String: String], regex: NSRegularExpression, file: IFile) throws {
         let content = try file.read()
         let matches = regex.matches(in: content, range: NSRange(content.startIndex..., in: content))
         guard matches.isNotEmpty else { return }
-        
+
         var newContent: String = ""
         var cursor = content.startIndex
         for match in matches {
             guard let range = Range(match.range, in: content) else { continue }
-            
+
             let prefix = content[cursor ..< range.lowerBound]
             newContent.append(contentsOf: prefix)
             cursor = range.upperBound
-            
+
             let lookup = String(content[range])
             if let replacement = replacements[lookup] {
                 newContent.append(contentsOf: replacement)
             }
         }
-        
+
         let suffix = content[cursor ..< content.endIndex]
         newContent.append(contentsOf: suffix)
         try file.write(newContent)
