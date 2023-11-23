901cfc927c37feef57a692abb3ca5ccb5851c422
Vyacheslav Khorkov <swiftyfinch@yandex.com>
2023-11-23 20:59:18 +0500

    Add tests for BinariesCleaner

--- Sources/RugbyFoundation/Core/Build/BinariesStorage/BinariesCleaner.swift
+++ Sources/RugbyFoundation/Core/Build/BinariesStorage/BinariesCleaner.swift
@@ -22,7 +22,7 @@ final class BinariesCleaner: Loggable {
     private let freeMultiplier = 0.4 // 40%
     private let formatter: ByteCountFormatter = {
         let formatter = ByteCountFormatter()
-        formatter.allowedUnits = [.useGB]
+        formatter.allowedUnits = [.useBytes, .useKB, .useMB, .useGB]
         return formatter
     }()
 
@@ -42,19 +42,19 @@ final class BinariesCleaner: Loggable {
 
     // MARK: - Private
 
-    private func calculateUsedSize() async throws -> Int? {
+    private func calculateUsedSize() async -> Int? {
         async let sharedSize = (try? Folder.at(sharedRugbyFolderPath).size()) ?? 0
         async let localSize = (try? Folder.at(localRugbyFolderPath).size()) ?? 0
         return await sharedSize + localSize
     }
 
-    private func calculateCleanUpInfo() async throws -> (
+    private func calculateCleanUpInfo() async -> (
         isCleanUpNeeded: Bool,
         used: Int,
         reserved: Int,
         spaceToFree: Int
     ) {
-        guard let used = try await calculateUsedSize() else { return (false, 0, 0, 0) }
+        guard let used = await calculateUsedSize() else { return (false, 0, 0, 0) }
         let reserved = Int(Double(used) * reserveMultiplier)
         let spaceToFree = max(0, used - Int(Double(limit) * (1 - freeMultiplier)))
         return (reserved >= limit, used, reserved, spaceToFree)
@@ -124,12 +124,11 @@ final class BinariesCleaner: Loggable {
 
 extension BinariesCleaner: IBinariesCleaner {
     func freeSpace() async throws {
-        let cleanUpInfo = try await log("Calculating Storage Info", level: .info, auto: await calculateCleanUpInfo())
+        let cleanUpInfo = await log("Calculating Storage Info", level: .info, auto: await calculateCleanUpInfo())
         let usedPercent = cleanUpInfo.used.percent(total: limit)
         await log("Used: \(formatter.string(fromByteCount: Int64(cleanUpInfo.used))) (\(usedPercent)%)", level: .info)
 
         guard cleanUpInfo.isCleanUpNeeded else { return }
-        await log("Used: \(formatter.string(fromByteCount: Int64(cleanUpInfo.used)))", level: .info)
         await log("Reserved: \(formatter.string(fromByteCount: Int64(cleanUpInfo.reserved)))", level: .info)
         await log("Limit: \(formatter.string(fromByteCount: Int64(limit)))", level: .info)
         await log("To Free: \(formatter.string(fromByteCount: Int64(cleanUpInfo.spaceToFree)))", level: .info)
@@ -140,13 +139,15 @@ extension BinariesCleaner: IBinariesCleaner {
             level: .info,
             auto: await selectFoldersForDeletion(in: candidatesForDeletion, spaceToFree: cleanUpInfo.spaceToFree)
         )
-        let logText = filesForDeletion.reduce(into: "Folders For Deletion:\n") { text, group in
-            text.append("\(group.key)\n")
-            text.append(group.value.reduce(into: "") { text, info in
-                text.append(info.delete ? "- \(info.path)\n" : "+ \(info.path)\n")
-            })
+        if filesForDeletion.isNotEmpty {
+            let logText = filesForDeletion.reduce(into: "Folders For Deletion:\n") { text, group in
+                text.append("\(group.key)\n")
+                text.append(group.value.reduce(into: "") { text, info in
+                    text.append(info.delete ? "- \(info.path)\n" : "+ \(info.path)\n")
+                })
+            }
+            await log(logText, level: .info)
         }
-        await log(logText, level: .info)
 
         try await log("Removing files", level: .info, block: {
             let pathsForDeletion = filesForDeletion.reduce(into: []) { paths, group in
@@ -172,7 +173,7 @@ extension BinariesCleaner: IBinariesCleaner {
         // Always remove build folder (DerivedData)
         try await removeBuildFolder()
 
-        if let used = try await calculateUsedSize() {
+        if let used = await calculateUsedSize() {
             let freed = cleanUpInfo.used - used
             await log("Freed: \(formatter.string(fromByteCount: Int64(freed)))", level: .info)
         }
