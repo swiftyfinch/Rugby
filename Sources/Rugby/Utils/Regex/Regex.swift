9cc80b543a58e9680222d2f71b99d81defdea0f1
Vyacheslav Khorkov <swiftyfinch@yandex.com>
2023-11-29 22:05:20 +0500

    Fix bug when target name has plus sign

--- Sources/Rugby/Utils/Regex/Regex.swift
+++ Sources/Rugby/Utils/Regex/Regex.swift
@@ -3,7 +3,7 @@ import RugbyFoundation
 
 func regex(patterns: [String], exactMatches: [String]) throws -> NSRegularExpression? {
     guard !patterns.isEmpty || !exactMatches.isEmpty else { return nil }
-    let exactMatches = exactMatches.map { "^\($0)$" }
+    let exactMatches = exactMatches.map { "^\(NSRegularExpression.escapedPattern(for: $0))$" }
     let joinedStrings = (patterns + exactMatches).joined(separator: "|")
     let regexString = "(" + joinedStrings + ")"
     return try NSRegularExpression(pattern: regexString, options: .anchorsMatchLines)
