65331d0b36a6878862590e3914a89466f0d029cf
Vyacheslav Khorkov <swiftyfinch@yandex.com>
2023-12-09 22:17:28 +0500

    Skip use command if targets weren't found

--- Sources/RugbyFoundation/Core/Use/UseBinariesManager.swift
+++ Sources/RugbyFoundation/Core/Use/UseBinariesManager.swift
@@ -147,6 +147,8 @@ extension UseBinariesManager: IUseBinariesManager {
             "Finding Build Targets",
             auto: await buildTargetsManager.findTargets(targetsRegex, exceptTargets: exceptTargetsRegex)
         )
+        guard binaryTargets.isNotEmpty else { return await log("Skip") }
+
         try await log("Backuping", level: .info, auto: await backupManager.backup(xcodeProject, kind: .original))
         try await librariesPatcher.patch(binaryTargets)
         try await log("Hashing Targets", auto: await targetsHasher.hash(binaryTargets, xcargs: xcargs))
