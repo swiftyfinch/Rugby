ffbbe1d0ed4e3985e08d3128bd384326feae0ef2
Vyacheslav Khorkov <swiftyfinch@yandex.com>
2024-02-10 22:31:20 +0500

    Reword target options

--- Sources/Rugby/Commands/Basic/Build/TargetsOptions.swift
+++ Sources/Rugby/Commands/Basic/Build/TargetsOptions.swift
@@ -1,19 +1,23 @@
 import ArgumentParser
 
 struct TargetsOptions: ParsableCommand {
-    @Option(name: .shortAndLong, parsing: .upToNextOption, help: "Targets for building. Empty means all targets.")
+    @Option(name: .shortAndLong,
+            parsing: .upToNextOption,
+            help: "Target names to select. Empty means all targets.")
     var targets: [String] = []
 
     @Option(name: [.long, .customShort("g")],
             parsing: .upToNextOption,
-            help: "Targets for building as a RegEx pattern.")
+            help: "Regular expression patterns to select targets. ")
     var targetsAsRegex: [String] = []
 
-    @Option(name: [.customLong("except"), .short], parsing: .upToNextOption, help: "Exclude targets from building.")
+    @Option(name: [.customLong("except"), .short],
+            parsing: .upToNextOption,
+            help: "Target names to exclude.")
     var exceptTargets: [String] = []
 
     @Option(name: [.long, .customShort("x")],
             parsing: .upToNextOption,
-            help: "Exclude targets from building as a RegEx pattern.")
+            help: "Regular expression patterns to exclude targets.")
     var exceptAsRegex: [String] = []
 }
