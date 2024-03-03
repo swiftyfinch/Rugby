f24a65134f5077da265e9b10fc147509bc4b058e
Vyacheslav Khorkov <swiftyfinch@yandex.com>
2024-03-03 19:32:56 +0500

    Add func to delete ~/.rugby/tests folder

--- Tests/FoundationTests/Core/Clear/CleanerTests.swift
+++ Tests/FoundationTests/Core/Clear/CleanerTests.swift
@@ -4,8 +4,9 @@ import XCTest
 
 final class CleanerTests: XCTestCase {
     private var sut: ICleaner!
-    private var sharedRugbyFolder: IFolder!
+    private var sharedBinariesFolder: IFolder!
     private var buildFolder: IFolder!
+    private var sharedTestsFolder: IFolder!
 
     override func setUp() async throws {
         try await super.setUp()
@@ -13,28 +14,32 @@ final class CleanerTests: XCTestCase {
         let testsFolderURL = tmp.appendingPathComponent(UUID().uuidString)
         let testsFolder = try Folder.create(at: testsFolderURL.path)
         buildFolder = try testsFolder.createFolder(named: ".rugby").createFolder(named: "build")
-        sharedRugbyFolder = try testsFolder.createFolder(named: ".rugby_shared")
+        let sharedRugbyFolder = try testsFolder.createFolder(named: ".rugby_shared")
+        sharedBinariesFolder = try sharedRugbyFolder.createFolder(named: "bin")
+        sharedTestsFolder = try sharedRugbyFolder.createFolder(named: "tests")
         sut = Cleaner(
-            sharedBinariesPath: sharedRugbyFolder.path,
-            buildFolderPath: buildFolder.path
+            sharedBinariesPath: sharedBinariesFolder.path,
+            buildFolderPath: buildFolder.path,
+            testsFolderPath: sharedTestsFolder.path
         )
     }
 
     override func tearDown() {
         super.tearDown()
         sut = nil
-        sharedRugbyFolder = nil
+        sharedBinariesFolder = nil
         buildFolder = nil
+        sharedTestsFolder = nil
     }
 }
 
 extension CleanerTests {
     func test_deleteSharedBinaries() async throws {
-        try sharedRugbyFolder.createFolder(named: "Alamofire")
+        try sharedBinariesFolder.createFolder(named: "Alamofire")
             .createFile(named: "Content", contents: "test_alamofire_content")
-        try sharedRugbyFolder.createFolder(named: "SnapKit")
+        try sharedBinariesFolder.createFolder(named: "SnapKit")
             .createFile(named: "Content", contents: "test_snapkit_content")
-        let moya = try sharedRugbyFolder.createFolder(named: "Moya")
+        let moya = try sharedBinariesFolder.createFolder(named: "Moya")
         let moyaContent = try moya.createFile(named: "Content", contents: "test_moya_content")
         let build = try buildFolder.createFile(named: "Content", contents: "test_build_content")
 
@@ -42,7 +47,10 @@ extension CleanerTests {
         try await sut.deleteSharedBinaries(names: ["Alamofire", "SnapKit", "Realm"])
 
         // Assert
-        let sharedFolders: [IItem] = try sharedRugbyFolder.folders(deep: true) + sharedRugbyFolder.files(deep: true)
+        let sharedFolders: [IItem] = try [
+            sharedBinariesFolder.folders(deep: true) as [IItem],
+            sharedBinariesFolder.files(deep: true) as [IItem]
+        ].flatMap { $0 }
         XCTAssertEqual(sharedFolders.count, 2)
         XCTAssertTrue(sharedFolders.map(\.path).contains(moya.path))
         XCTAssertTrue(sharedFolders.map(\.path).contains(moyaContent.path))
@@ -52,16 +60,16 @@ extension CleanerTests {
     }
 
     func test_deleteAllSharedBinaries() async throws {
-        try sharedRugbyFolder.createFolder(named: "Alamofire").createFolder(named: "Content")
-        try sharedRugbyFolder.createFolder(named: "SnapKit").createFolder(named: "Content")
-        try sharedRugbyFolder.createFolder(named: "Moya").createFolder(named: "Content")
+        try sharedBinariesFolder.createFolder(named: "Alamofire").createFolder(named: "Content")
+        try sharedBinariesFolder.createFolder(named: "SnapKit").createFolder(named: "Content")
+        try sharedBinariesFolder.createFolder(named: "Moya").createFolder(named: "Content")
         let build = try buildFolder.createFolder(named: "Content")
 
         // Act
         try await sut.deleteAllSharedBinaries()
 
         // Assert
-        let sharedFolders = try sharedRugbyFolder.folders(deep: true)
+        let sharedFolders = try sharedBinariesFolder.folders(deep: true)
         XCTAssertTrue(sharedFolders.isEmpty)
         let buildFolders = try buildFolder.folders(deep: true)
         XCTAssertEqual(buildFolders.count, 1)
@@ -70,18 +78,34 @@ extension CleanerTests {
 
     func test_deleteBuildFolder() async throws {
         try buildFolder.createFolder(named: "Content")
-        let moya = try sharedRugbyFolder.createFolder(named: "Moya")
+        let moya = try sharedBinariesFolder.createFolder(named: "Moya")
         let moyaContent = try moya.createFolder(named: "Content")
 
         // Act
         try await sut.deleteBuildFolder()
 
         // Assert
-        let sharedFolders = try sharedRugbyFolder.folders(deep: true)
+        let sharedFolders = try sharedBinariesFolder.folders(deep: true)
         XCTAssertEqual(sharedFolders.count, 2)
         XCTAssertTrue(sharedFolders.map(\.path).contains(moya.path))
         XCTAssertTrue(sharedFolders.map(\.path).contains(moyaContent.path))
         let buildFolders = try buildFolder.folders(deep: true)
         XCTAssertTrue(buildFolders.isEmpty)
+    }
+
+    func test_deleteTestsFolder() async throws {
+        try sharedTestsFolder
+            .createFolder(named: "LocalPod-Unit-Tests/Debug-iphonesimulator-arm64/fe01fae")
+            .createFile(named: "fe01fae.yml")
+        try sharedTestsFolder
+            .createFolder(named: "LocalPod-Unit-ResourceBundleTests/Debug-iphonesimulator-arm64/f0d6226")
+            .createFile(named: "f0d6226.yml")
+
+        // Act
+        try await sut.deleteTestsFolder()
+
+        // Assert
+        let testsFolders = try sharedTestsFolder.folders(deep: false)
+        XCTAssertTrue(testsFolders.isEmpty)
     }
 }
