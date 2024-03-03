c3fe18fa1c11b235d9c74f679130af2783afb1c2
Vyacheslav Khorkov <swiftyfinch@yandex.com>
2024-03-03 22:38:39 +0500

    Update test scripts

--- Example/Tests/cache_tests.sh
+++ Example/Tests/cache_tests.sh
@@ -15,24 +15,21 @@ rugby build -t Pods-ExampleLibs --output multiline
 rugby use -t Pods-ExampleFrameworks Pods-ExampleLibs --output multiline
 
 header "Test"
-SIMULATOR_ID=`xcrun simctl list devices | grep -m 1 'iPhone 14 (' | grep -oE '[0-9A-Z\-]{20,}'`
-xcrun simctl shutdown all
-xcrun simctl boot $SIMULATOR_ID
 set -o pipefail && env NSUnbufferedIO=YES arch -arm64 xcodebuild test \
   -workspace Example.xcworkspace \
   -scheme ExampleFrameworks \
-  -configuration Debug \
-  -destination "platform=iOS Simulator,id=${SIMULATOR_ID}" \
-  -derivedDataPath build \
+  -testPlan ExampleFrameworks \
+  -destination "platform=iOS Simulator,name=iPhone 15" \
   COMPILER_INDEX_STORE_ENABLE=NO \
   SWIFT_COMPILATION_MODE=wholemodule \
+  CODE_SIGNING_ALLOWED=NO \
   | xcbeautify
 set -o pipefail && env NSUnbufferedIO=YES arch -arm64 xcodebuild test \
   -workspace Example.xcworkspace \
   -scheme ExampleLibs \
-  -configuration Debug \
-  -destination "platform=iOS Simulator,id=${SIMULATOR_ID}" \
-  -derivedDataPath build \
+  -testPlan ExampleLibs \
+  -destination "platform=iOS Simulator,name=iPhone 15" \
   COMPILER_INDEX_STORE_ENABLE=NO \
   SWIFT_COMPILATION_MODE=wholemodule \
+  CODE_SIGNING_ALLOWED=NO \
   | xcbeautify
