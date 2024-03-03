da74f5d29831ac7c74db0ed8f982b2e0e5097055
Vyacheslav Khorkov <swiftyfinch@yandex.com>
2024-03-03 22:40:18 +0500

    Add test scripts for rugby test command

---
+++ Example/Tests/cache_tests_multiproj.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+header() {
+	printf "\e[33m>\e[m \e[32mStep: $1\e[m\n"
+}
+
+header "CocoaPods"
+MULTIPLE_PROJECTS=true pod install --repo-update
+
+header "Rugby Test"
+rugby _test -g framework \
+            -n 'iPhone 15' \
+            -p ExampleFrameworks \
+            --output multiline
