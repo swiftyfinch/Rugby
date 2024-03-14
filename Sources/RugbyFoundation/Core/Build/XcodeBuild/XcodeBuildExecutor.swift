4b8dd97a925a33b0c3d2263c8cdef19ee9477815
Vyacheslav Khorkov <swiftyfinch@yandex.com>
2024-03-14 23:41:53 +0500

    Add creating symlink to the latest logs folder

--- Sources/RugbyFoundation/Core/Build/XcodeBuild/XcodeBuildExecutor.swift
+++ Sources/RugbyFoundation/Core/Build/XcodeBuild/XcodeBuildExecutor.swift
@@ -30,7 +30,7 @@ final class XcodeBuildExecutor: IXcodeBuildExecutor, Loggable {
     }
 
     func run(_ command: String, rawLogPath: String, logPath: String, args: Any...) async throws {
-        try Folder.create(at: URL(fileURLWithPath: rawLogPath).deletingLastPathComponent().path)
+        try createRawLogFolder(at: rawLogPath)
         try shellExecutor.throwingShell(command, args: args, "| tee '\(rawLogPath)'")
         try await log("Beautifying Log", block: {
             if let errors = try? await beautifyLog(rawLogPath: rawLogPath, logPath: logPath), errors.isNotEmpty {
@@ -40,6 +40,14 @@ final class XcodeBuildExecutor: IXcodeBuildExecutor, Loggable {
     }
 
     // MARK: - Private
+
+    private func createRawLogFolder(at rawLogPath: String) throws {
+        let folder = try Folder.create(at: URL(fileURLWithPath: rawLogPath).deletingLastPathComponent().path)
+        guard let parent = folder.parent else { return }
+        let latestLogFolderSymlink = "\(parent.path)/+latest"
+        shellExecutor.shell("rm -f \(latestLogFolderSymlink)")
+        try FileManager.default.createSymbolicLink(atPath: latestLogFolderSymlink, withDestinationPath: folder.path)
+    }
 
     private func beautifyLog(rawLogPath: String, logPath: String) async throws -> [String] {
         var tests: [String] = []
