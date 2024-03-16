3f4f185b7e8b592f2a4cea3dfcc4113f2acbed68
Vyacheslav Khorkov <swiftyfinch@yandex.com>
2024-03-16 19:19:17 +0500

    Add script to update docs

---
+++ .github/scripts/update_docs.rb
@@ -0,0 +1,24 @@
+DOCS_GLOB = 'Docs/**/*.md'
+SHELL_BLOCK_REGEX = "```sh\n(.*?)\n```"
+COMMAND_PREFIX = '> '
+
+Dir.glob(DOCS_GLOB).each do |file|
+  content = File.read(file)
+  next unless matches = content.scan(/#{SHELL_BLOCK_REGEX}/m) and matches.length > 1
+
+  command = matches[0][0].to_s
+  next unless command.start_with?(COMMAND_PREFIX)
+  command = command.delete_prefix(COMMAND_PREFIX)
+
+  current_help = matches[1][0].to_s
+  help = `#{command}`
+  help.sub!(/\si\s.*/m, '') # Remove a block with links
+  help.sub!(/╭─*╮\n│\s*│[\s\S]*?╯\n\n/, '') # Remove the header with version
+
+  help.delete_suffix!("\n\n")
+
+  next if current_help == help
+  updated_content = content.gsub(current_help, help)
+  File.write(file, updated_content)
+  puts "Updated: #{file}"
+end
