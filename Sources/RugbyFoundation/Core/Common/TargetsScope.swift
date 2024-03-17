654e06193b83e1e308ac8d35c9339070a2a024e5
Vyacheslav Khorkov <swiftyfinch@yandex.com>
2024-03-17 19:03:32 +0500

    Add a new flag --try

--- Sources/RugbyFoundation/Core/Common/TargetsScope.swift
+++ Sources/RugbyFoundation/Core/Common/TargetsScope.swift
@@ -1,6 +1,41 @@
 import Foundation
 
+/// A set of options to to select targets.
+public struct TargetsOptions {
+    /// A mode where only the selected targets are printed.
+    var tryMode: Bool
+    /// A RegEx to select targets.
+    var targetsRegex: NSRegularExpression?
+    /// A RegEx to exclude targets.
+    var exceptTargetsRegex: NSRegularExpression?
+
+    /// - Parameters:
+    ///   - tryMode: A mode where only the selected targets are printed.
+    ///   - targetsRegex: A RegEx to select targets.
+    ///   - exceptTargetsRegex: A RegEx to exclude targets.
+    public init(
+        tryMode: Bool = false,
+        targetsRegex: NSRegularExpression? = nil,
+        exceptTargetsRegex: NSRegularExpression? = nil
+    ) {
+        self.tryMode = tryMode
+        self.targetsRegex = targetsRegex
+        self.exceptTargetsRegex = exceptTargetsRegex
+    }
+}
+
+// MARK: - TargetsScope
+
 enum TargetsScope {
     case exact(TargetsMap)
     case filter(regex: NSRegularExpression?, exceptRegex: NSRegularExpression?)
+}
+
+extension TargetsScope {
+    init(_ targetsOptions: TargetsOptions) {
+        self = .filter(
+            regex: targetsOptions.targetsRegex,
+            exceptRegex: targetsOptions.exceptTargetsRegex
+        )
+    }
 }
