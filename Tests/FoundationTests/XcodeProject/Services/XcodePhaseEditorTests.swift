fe8aa4e5a2551c4f8b719822758c650e434ee61e
Vyacheslav Khorkov <swiftyfinch@yandex.com>
2024-03-26 23:21:56 +0500

    Add methods to work with XCFramework build phase

--- Tests/FoundationTests/XcodeProject/Services/XcodePhaseEditorTests.swift
+++ Tests/FoundationTests/XcodeProject/Services/XcodePhaseEditorTests.swift
@@ -106,4 +106,66 @@ extension XcodePhaseEditorTests {
         expectedAggregateTargetBuildPhases.remove(at: 1)
         XCTAssertEqual(resultAggregateTargetBuildPhases, expectedAggregateTargetBuildPhases)
     }
+
+    func test_filterXCFrameworksPhaseTargets() {
+        let target0 = IInternalTargetMock()
+        target0.underlyingName = "target0_name"
+        target0.underlyingUuid = "target0_uuid"
+        target0.buildPhases = [BuildPhase(name: "Some Script", type: .runScript)]
+        let target1 = IInternalTargetMock()
+        target1.underlyingName = "target1_name"
+        target1.underlyingUuid = "target1_uuid"
+        target1.buildPhases = [BuildPhase(name: "[CP] Copy XCFrameworks", type: .runScript)]
+        let target2 = IInternalTargetMock()
+        target2.underlyingName = "target2_name"
+        target2.underlyingUuid = "target2_uuid"
+        target2.buildPhases = [BuildPhase(name: "Sources", type: .sources)]
+
+        // Act
+        let filteredTargets = sut.filterXCFrameworksPhaseTargets([
+            target0.uuid: target0,
+            target1.uuid: target1,
+            target2.uuid: target2
+        ])
+
+        // Assert
+        XCTAssertEqual(filteredTargets.count, 1)
+        XCTAssertEqual(filteredTargets.first?.value.name, target1.name)
+    }
+
+    func test_copyXCFrameworksPhase() {
+        let target0 = IInternalTargetMock()
+        target0.underlyingName = "target0_name"
+        target0.underlyingUuid = "target0_uuid"
+        target0.underlyingPbxTarget = PBXTarget(name: "target0_name")
+        let project = IProjectMock()
+        project.pbxProj = PBXProj()
+        target0.underlyingProject = project
+        let target1 = IInternalTargetMock()
+        target1.underlyingName = "target1_name"
+        target1.underlyingUuid = "target1_uuid"
+        target1.underlyingPbxTarget = PBXTarget(name: "target1_name")
+        let xcframeworkBuildPhase = PBXShellScriptBuildPhase(
+            files: [],
+            name: "[CP] Copy XCFrameworks",
+            inputPaths: ["inputPaths_test"],
+            outputPaths: ["outputPaths_test"],
+            inputFileListPaths: ["inputFileListPaths_test"],
+            outputFileListPaths: ["outputFileListPaths_test"],
+            shellPath: "shellPath_test",
+            shellScript: "shellScript_test",
+            buildActionMask: 99,
+            runOnlyForDeploymentPostprocessing: false,
+            showEnvVarsInLog: false,
+            alwaysOutOfDate: false,
+            dependencyFile: "dependencyFile_test"
+        )
+        target1.pbxTarget.buildPhases = [xcframeworkBuildPhase]
+
+        // Act
+        sut.copyXCFrameworksPhase(from: target1, to: target0)
+
+        // Assert
+        XCTAssertEqual(target0.pbxTarget.buildPhases.first, xcframeworkBuildPhase)
+    }
 }
