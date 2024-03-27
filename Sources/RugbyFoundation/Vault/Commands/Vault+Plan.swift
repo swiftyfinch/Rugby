b894f20291e5887afdd15ad3a5c9cb8735694f4c
Vyacheslav Khorkov <swiftyfinch@yandex.com>
2024-03-27 22:54:39 +0500

    Add resolving env variables in plans

--- Sources/RugbyFoundation/Vault/Commands/Vault+Plan.swift
+++ Sources/RugbyFoundation/Vault/Commands/Vault+Plan.swift
@@ -1,4 +1,4 @@
 public extension Vault {
     /// The service to parse YAML files with Rugby plans.
-    var plansParser: IPlansParser { PlansParser() }
+    var plansParser: IPlansParser { PlansParser(envVariablesResolver: envVariablesResolver) }
 }
